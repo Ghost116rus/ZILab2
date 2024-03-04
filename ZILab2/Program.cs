f61702eef9c7918a7faf59e0e1e3b3dff8758189
Ghost116rus <danilkaleev@mai.ru>
2024-03-04 23:51:14 +0300

    Добавьте файлы проекта.

---
+++ ZILab2/Program.cs
@@ -0,0 +1,58 @@
+﻿using ZILab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int SubjectsInSystem = 8;
+        int objectsForAccess = 3;
+
+        List<string> names = new List<string>() { "Дамир", "Андрей", "Булат", "Айнур", "Русик", "Марсель", "Коля" };
+        List<User> Users = new List<User>() { };
+        List<string> data = new List<string>() { };
+
+        string AdminName = "Данил";
+        int[] AdminAccess = new int[objectsForAccess];
+
+        for (int i = 0; i < objectsForAccess; i++) 
+            AdminAccess[i] = 7;
+
+        Users.Add(new User(AdminName, AdminAccess, objectsForAccess));
+        data.Add(AdminName);
+
+        Random tim = new Random();
+        for (int i = 0; i < (SubjectsInSystem - 1); i++)
+        {
+            int[] UserAccess = new int[objectsForAccess];
+            for (int j = 0; j < objectsForAccess; j++)
+            {
+                UserAccess[j] = tim.Next(0, 7);
+            }
+            string UserName = names[i];
+            Users.Add(new User(UserName, UserAccess, objectsForAccess));
+            data.Add(UserName);
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string enter_name = Console.ReadLine();
+            if (Users.Any(x => x.Name == enter_name))
+            {
+                Console.WriteLine("Идентификация прошла успешно. Добро пожаловать, " + enter_name + ". \n");
+                var user = Users.FirstOrDefault(x => x.Name == enter_name);
+                user?.right();
+                user?.direct(Users);
+            }
+            else if (enter_name == "Выход")
+            {
+                Console.Clear();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Такого пользователя нет \n");
+            }
+        }
+    }
+}
---
+++ ZILab2/User.cs
@@ -0,0 +1,151 @@
+﻿namespace ZILab2
+{
+    class User
+    {
+        private string _name;
+        private int[] _access;
+        private int _objectsForAccess;
+
+        public User(string name, int[] access, int O)
+        {
+            this._name = name;
+            this._access = access;
+            this._objectsForAccess = O;
+        }
+
+        public string Name { get =>  _name; }
+
+        public void modif(int num, string ri)
+        {
+            if (ri == "Чтение")
+            {
+                this._access[num] = this._access[num] | 4;
+            }
+
+            if (ri == "Запись")
+            {
+                this._access[num] = this._access[num] | 2;
+            }
+
+            if (ri == "Передача прав")
+            {
+                this._access[num] = this._access[num] | 1;
+            }
+        }
+
+        public void right()
+        {
+            Console.WriteLine("Ваши права");
+            for (int i = 0; i < this._objectsForAccess; i++)
+            {
+                int x = this._access[i];
+                int read = (x / (int)Math.Pow(2, (3 - 1))) % 2;
+                int write = (x / (int)Math.Pow(2, (2 - 1))) % 2;
+                int rgh = (x / (int)Math.Pow(2, (1 - 1))) % 2;
+                if (read == 1 && write == 1 && rgh == 1)
+                {
+                    Console.WriteLine("Файл" + (i + 1) + ": Полные права");
+                }
+                else
+                {
+                    string rez = "Файл" + (i + 1).ToString() + ": ";
+                    if (read == 1) rez += "Чтение. ";
+                    if (write == 1) rez += "Запись. ";
+                    if (rgh == 1) rez += "Передача прав. ";
+                    if (read == 0 && write == 0 && rgh == 0) rez += "Запрет.";
+                    Console.WriteLine(rez);
+                }
+            }
+        }
+
+        public void direct(List<User> obj)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите команду (Чтение, Запись, Передача прав, Выход): ");
+                    string command = Console.ReadLine();
+                    if (command == "Чтение")
+                    {
+                        Console.WriteLine("Введите номер файла: ");
+                        int num_obj = Convert.ToInt32(Console.ReadLine());
+                        int x = this._access[num_obj - 1];
+                        int read = (x / (int)Math.Pow(2, (3 - 1))) % 2;
+                        if (read == 1)
+                        {
+                            Console.WriteLine("Операция прошла успешно. \n");
+                        }
+                        else Console.WriteLine("Отказ в выполнении операции. Вы не обладаете соответствующими правами. \n");
+                    }
+
+                    else if (command == "Запись")
+                    {
+                        Console.WriteLine("Введите номер файла: ");
+                        int num_obj = Convert.ToInt32(Console.ReadLine());
+                        int x = this._access[num_obj - 1];
+                        int write = (x / (int)Math.Pow(2, (2 - 1))) % 2;
+                        if (write == 1)
+                        {
+                            Console.WriteLine("Операция прошла успешно. \n");
+                        }
+                        else Console.WriteLine("Отказ в выполнении операции. Вы не обладаете соответствующими правами. \n");
+                    }
+                    else if (command == "Передача прав")
+                    {
+                        Console.WriteLine("Право на какой объект передаётся?");
+                        int num_obj = Convert.ToInt32(Console.ReadLine());
+                        int x = this._access[num_obj - 1];
+                        int rgh = (x / (int)Math.Pow(2, (1 - 1))) % 2;
+                        if (rgh == 1)
+                        {
+                            Console.WriteLine("Какое право передаётся?");
+                            string ri = Console.ReadLine();
+                            int read = (x / (int)Math.Pow(2, (3 - 1))) % 2;
+                            int write = (x / (int)Math.Pow(2, (2 - 1))) % 2;
+                            if ((ri == "Чтение" && read == 1) || (ri == "Запись" && write == 1) || (ri == "Передача прав" && rgh == 1))
+                            {
+                                Console.WriteLine("Какому пользователю передаётся право? Введите имя пользователя: ");
+                                string recievedRightsUserName = Console.ReadLine();
+                                foreach (User user in obj)
+                                {
+                                    if (recievedRightsUserName == user.Name)
+                                    {
+                                        user.modif(num_obj - 1, ri);
+                                        Console.WriteLine("Передача произошла успешно. \n");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Невозможно передать права, которых нет. \n");
+                            }
+                        }
+                        else if (rgh == 0)
+                        {
+                            Console.WriteLine("Отказ в выполнении операции. Вы не обладаете соответствующими правами. \n");
+                        }
+
+                    }
+
+                    else if (command == "Выход")
+                    {
+                        Console.WriteLine("Осуществлён выход из системы. Всего доброго, " + this._name + ". \n\n");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод. \n");
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод. \n");
+                }
+            }
+        }
+
+    }
+}
